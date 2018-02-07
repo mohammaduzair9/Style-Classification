57702125c7306ca6a352950afd1e2762959d8802
mohammaduzair9 <mohduzair9@gmail.com>
2018-02-08 01:29:49 +0500

    Added install.sh to install all requirements using script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+pip install --upgrade tensorflow-gpu 
+pip install h5py
+pip install numpy
+pip install opencv-python
+pip install sklearn
+pip install Keras==2.0.3 
